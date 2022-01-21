e0b25b478c2452f33b2f649f141fbd4f45f93892
Slaven B <slaven.bunijevac@gmail.com>
2022-01-21 21:28:39 +0100

    introduce Events component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import { useState } from 'react'
 import './App.css'
 
 import People from './components/People';
 import Events from './components/Events';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
