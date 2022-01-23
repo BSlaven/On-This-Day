4708580998e168debf4e3ee2eaf21484f63bd0f8
Slaven B <slaven.bunijevac@gmail.com>
2022-01-23 12:42:32 +0100

    add introduce person card basic structure

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -3,7 +3,9 @@ import classes from './Person.module.css'
 const Person = () => {
   return (
     <div className={classes.card}>
-      Slaven Bunijevac
+      <h4 className={classes.name}>Slaven Bunijevac</h4>
+      <p className={classes.description}>Amazing developer and tranlator</p>
+      <button className={classes.more}>Learn more</button>
     </div>
   );
 };
