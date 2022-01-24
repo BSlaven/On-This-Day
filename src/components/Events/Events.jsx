a4e7fc6b1c4489e8c79798d10cc2f81b9cd034de
Slaven B <slaven.bunijevac@gmail.com>
2022-01-24 18:17:01 +0100

    add form to events component

--- src/components/Events/Events.jsx
+++ src/components/Events/Events.jsx
@@ -1,9 +1,11 @@
+import DateForm from '../DateForm/DateForm';
 import classes from './Events.module.css';
 import Event from '../Event/Event';
 
 const Events = () => {
   return (
     <div className={classes.events}>
+      <DateForm />
       <Event />
       <Event />
       <Event />
