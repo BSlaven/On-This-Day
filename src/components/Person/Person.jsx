da738b1dfcb95540ae0cf290a24f68913ba9567b
Slaven B <slaven.bunijevac@gmail.com>
2022-01-25 16:06:49 +0100

    show persons in a list

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,11 +1,14 @@
 import classes from './Person.module.css'
 
-const Person = () => {
+const Person = ({ person }) => {
+
+  const link = person.wikipedia[0].wikipedia;
+
   return (
     <div className={classes.card}>
-      <h4 className={classes.name}>Slaven Bunijevac</h4>
-      <p className={classes.description}>Amazing developer and translator</p>
-      <a className={classes.more}>Learn more...</a>
+      <h4 className={classes.name}>{person.year}</h4>
+      <p className={classes.description}>{person.description}</p>
+      <a href={link} target='_blank' className={classes.more}>Learn more...</a>
     </div>
   );
 };
