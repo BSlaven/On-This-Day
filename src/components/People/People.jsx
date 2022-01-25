fd78a0902bd788c1512f580703a23a676274ce53
Slaven B <slaven.bunijevac@gmail.com>
2022-01-25 14:21:41 +0100

    get births into People component

--- src/components/People/People.jsx
+++ src/components/People/People.jsx
@@ -9,11 +9,14 @@ const People = () => {
   const [people, setPeople] = useState([]);
 
   const listAllPeople = persons => {
+    console.log(persons.births);
     setPeople(persons);
   }
 
   return (
     <div className={classes.people}>
+      <h3 className={classes.peopleTitle}>People born on this date</h3>
+      <h4 className={classes.date}>({people.date})</h4>
       <DateForm section='births' getPeople={listAllPeople} />
       <Person />
       <Person />
