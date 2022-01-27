030effccf1a899f33e45dd548b56ac1c08f7fbe1
Slaven B <slaven.bunijevac@gmail.com>
2022-01-27 22:39:42 +0100

    introduce load more button

---
+++ src/components/LoadMoreBtn/LoadMoreBtn.jsx
@@ -0,0 +1,9 @@
+const LoadMoreBtn = () => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default LoadMoreBtn;
--- src/components/People/People.jsx
+++ src/components/People/People.jsx
@@ -8,18 +8,21 @@ const People = () => {
 
   const [peopleData, setPeopleData] = useState([]);
   const [currentDate, setCurrentDate] = useState('');
+  const [ showLimit, setShowLimit ] = useState(5);
 
   const listAllPeople = persons => {
     setPeopleData(persons.births);
     setCurrentDate(persons.date);
   }
 
+  const peopleToShow = peopleData.slice(0, showLimit);
+
   return (
     <div className={classes.people}>
       <h3 className={classes.peopleTitle}>People born on this date</h3>
       <h4 className={classes.date}>({currentDate})</h4>
       <DateForm section='births' getData={listAllPeople} />
-      {peopleData && peopleData.map(person => <Person key={person.description} person={person} />)}
+      {peopleData && peopleToShow.map(person => <Person key={person.description} person={person} />)}
     </div>
   );
 };
