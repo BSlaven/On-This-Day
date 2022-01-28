02d6083e8c9cee89df2c63c5db885988eca6b5ad
Slaven B <slaven.bunijevac@gmail.com>
2022-01-28 11:45:47 +0100

    introduce jsx and css file for the button

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import './App.css'
 
 import People from './components/People/People';
 import Events from './components/Events/Events';
+import ToTopBtn from './components/ToTopBtn/ToTopBtn'
 
 function App() {
 
   return (
     <div className="App">
+      <ToTopBtn />
       <h2 className="main-header">On This Day</h2>
       <div className="container">
         <People />
---
+++ src/components/ToTopBtn/ToTopBtn.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ToTopBtn = () => {
+  return (
+    <div>
+      button
+    </div>
+  );
+};
+
+export default ToTopBtn;
