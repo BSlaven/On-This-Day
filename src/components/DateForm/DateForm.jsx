74ce17df29b2ce0e267aa8509727ca4e0bfa764b
Slaven B <slaven.bunijevac@gmail.com>
2022-01-28 22:07:51 +0100

    introduced input type date

--- src/components/DateForm/DateForm.jsx
+++ src/components/DateForm/DateForm.jsx
@@ -4,22 +4,18 @@ import classes from './DateForm.module.css';
 
 const DateForm = ({ section, getData }) => {
 
-  const [day, setDay] = useState('');
-  const [month, setMonth] = useState('');
+  const [ day, setDay ] = useState('');
+  const [ month, setMonth ] = useState('');
+  const [ date, setDate ] = useState('');
 
   const dayInputHandler = e => {
-    const value = e.target.value.trim();
-    console.log(value);
-    // if(parseInt(value) < 1 || parseInt(value) > 31) return;
-    // if(month == 2 && parseInt(value) > 29) return;
-    // if((month == 6 || month == 4 || month == 9 || month == 11) && parseInt(value) > 30) return;
-    setDay(value);
-  }
-
-  const monthInputHandler = e => {
-    const value = e.target.value.trim();
-    if (parseInt(value) < 1 || parseInt(value) > 12) return;
-    setMonth(value);
+    const value = e.target.value;
+    setDate(value);
+    const myDate = new Date(value);
+    const myDay = myDate.getDate();
+    const myMonth = myDate.getMonth() + 1;
+    setDay(myDay);
+    setMonth(myMonth);
   }
 
   const fetchData = async (day, month) => {
@@ -44,8 +40,8 @@ const DateForm = ({ section, getData }) => {
       <div className={classes.date}>
         <label htmlFor="day-input">Date</label>
         <input
-          value={day || ''}
-          onChange={dayInputHandler}
+          value={date || ''}
+          onInput={dayInputHandler}
           type="date"
           name="day-input"
           id="day-input" />
